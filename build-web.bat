39b17bea59d2929302e552c49e98472f0339362c
minufy <seomeenu@proton.me>
2026-05-07 20:41:24 +0900

    asassasasa

--- build-web.bat
+++ build-web.bat
@@ -1,2 +1,2 @@
 bandizip a game.love assets modules objects scenes stuff conf.lua main.lua settings.lua
-npx love.js.cmd game.love lovejs/ -t "replicate" 
+npx love.js.cmd game.love lovejs/ -c -t "replicate!"
